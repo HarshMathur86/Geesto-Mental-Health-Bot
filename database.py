31f591aa01ce287bad9a62652e0b99a9ba15688c
Harsh Mathur <60878060+HarshMathur86@users.noreply.github.com>
2022-11-16 21:18:06 +0530

    file extractor query added

--- database.py
+++ database.py
@@ -9,17 +9,18 @@ logger = logging.getLogger(__name__)
 
 
 # Initialising database login credentials
-DB_HOST = "SAMPLE"
-DB_NAME = "SAMPLE"
-DB_USER = "SAMPLE"
-DB_PASS = "SAMPLE"
+DB_HOST = "SAMPlE"
+DB_NAME = "SAMPlE"
+DB_USER = "SAMPlE"
+DB_PASS = "SAMPlE"
+DB_PORT = 0000
 
 def execute_query(query):
     
     # Connecting with the database
     #logger.info("connecting with database & executing the query")
 
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
 
     with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
         try:
@@ -38,6 +39,40 @@ def execute_query(query):
             # when no result to be fetched like the insert query
             data = None
             logger.info("data added/updated on database")
+
+        except Exception as exp:
+            pass
+        
+    conn.commit()
+    conn.close()
+    
+    return data
+
+def query_result_file_extractor(query, filename):
+    
+    # Connecting with the database
+    #logger.info("connecting with database & executing the query")
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    
+    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+        try:
+            # Executing the query
+            with open("Resources/Database CSVs/" + filename, "w") as file:
+                cur.copy_expert("copy ({}) to stdout with csv header".format(query), file)
+
+        except Exception as exp:
+            logger.info("error occured in database query")
+            logging.error(exp)
+        
+        try:
+            data = cur.fetchall()
+            logger.info("data fetched from database")
+            
+        except psycopg2.ProgrammingError:
+            # when no result to be fetched like the insert query
+            data = None
+            logger.info("file  \"{}\" created".format(filename))
 
         except Exception as exp:
             pass
